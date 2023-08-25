93ea48a3585f5f0aadffa20ffbc5cfa49c5f0ea7
WaydinJochem <jochem7405@gmail.com>
2023-08-25 16:29:45 +0200

    Sort

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,14 +1,52 @@
 <template>
   <div>
+    <div>
+      <!--
+      <input type="search">
+      <button><select name="" id="">
+        <option value="">Ascending</option>
+        <option value="">Descending</option>
+      </select></button>
+    </div>
+    <div> -->
+
+      <!-- sorting -->
+      <input type="search" >
+    </div>
     <CardComp />
   </div>
 </template>
 <script>
 import CardComp from '@/components/Card.vue'
+import { reactive, ref } from 'vue'
 export default {
   components:
   {
-    CardComp
-  }
+    CardComp,
+    // filters: {
+    //   capitalize: function (value) {
+    //     if (!value) return ''
+    //     value = value.toString()
+    //     return value.charAt(0).toUpperCase() + value.slice(1)
+    //   }
+    // }
+  }, 
+  computed: {
+    sortedArray() {
+      let sortedProducts = this.products;
+
+      sortedProducts = sortedProducts.sort((a,b)=>{
+        let fa = a.title.lowerCase(), fb = b.title.toLowerCase();
+        if(fa < fb) {
+          return -1
+        }
+        if(fa > fb) {
+          return 1
+        }
+        return 0 
+      })
+    }
+  },
+ 
 }
 </script>
